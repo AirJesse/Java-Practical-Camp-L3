e59ab9fc48a53316ff8a32288a356a21f534d678
lujiachao <me@ljc.im>
2024-11-22 15:57:22 +0800

    u

---
+++ springBoot-shiro-outh2-productor/src/main/java/org/bank/common/config/CustomerCorsFilter.java
@@ -0,0 +1,29 @@
+package org.bank.common.config;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CustomerCorsFilter extends CorsFilter {
+
+    public CustomerCorsFilter( ) {
+        super(corsConfigurationSource());
+    }
+
+    private static CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // 允许访问的域名，例如 http://localhost:8080
+        configuration.addAllowedHeader("*"); // 允许所有请求头
+        configuration.addAllowedMethod("*"); // 允许所有请求方法
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}

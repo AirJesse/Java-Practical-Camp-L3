acfa9d5c336c39f4ebf62fc85c9fb3a75083c8bd
lujiachao <me@ljc.im>
2024-11-22 15:57:00 +0800

    add login

---
+++ login/src/main/java/com/lujiachao/login/codeGenerator/CodeGenerator.java
@@ -0,0 +1,31 @@
+package com.lujiachao.login.codeGenerator;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.nio.file.Paths;
+
+public class CodeGenerator {
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:mysql://192.168.120.33:3306/login_test?serverTimezone=Asia/Shanghai&useUnicode=true&characterEncoding=utf-8", "l3camp", "camp123456")
+                .globalConfig(builder -> builder
+                        .author("lujiachao")
+                        .outputDir(Paths.get(System.getProperty("user.dir")) + "/src/main/java")
+                        .commentDate("yyyy-MM-dd")
+                )
+                .packageConfig(builder -> builder
+                        .parent("com.lujiachao.login")
+                        .entity("entity")
+                        .mapper("mapper")
+                        .service("service")
+                        .serviceImpl("service.impl")
+                        .xml("mapper.xml")
+                )
+                .strategyConfig(builder -> builder
+                        .entityBuilder()
+                        .enableLombok()
+                )
+                .templateEngine(new FreemarkerTemplateEngine())
+                .execute();
+    }
+}
